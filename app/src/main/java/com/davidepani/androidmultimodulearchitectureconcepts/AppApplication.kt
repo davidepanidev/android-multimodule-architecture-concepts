24fed35a8946b27cf3683e0f01e7042d6adb99eb
davide-pani <102189224+davide-pani@users.noreply.github.com>
2022-03-27 12:54:03 +0200

    [app] Added hilt

---
+++ app/src/main/java/com/davidepani/androidmultimodulearchitectureconcepts/AppApplication.kt
@@ -0,0 +1,8 @@
+package com.davidepani.androidmultimodulearchitectureconcepts
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AppApplication : Application() {
+}

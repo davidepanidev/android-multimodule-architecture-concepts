729d4bf40dfa5600bd140d4b0a5c41f4f303bb28
davide-pani <102189224+davide-pani@users.noreply.github.com>
2022-03-28 20:01:47 +0200

    added ktx android and fragment libraries in order to inject the viewmodels using by viewModels()

--- presentation/src/main/java/com/davidepani/presentation/ui/coin/CoinFragment.kt
+++ presentation/src/main/java/com/davidepani/presentation/ui/coin/CoinFragment.kt
@@ -1,10 +1,8 @@
 package com.davidepani.presentation.ui.coin
 
-import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
 import androidx.viewbinding.ViewBinding
 import com.davidepani.architectures.viewbinding.BaseViewBindingHandlerFragment
 import com.davidepani.presentation.databinding.CoinFragmentBinding
@@ -14,16 +12,11 @@ import dagger.hilt.android.AndroidEntryPoint
 class CoinFragment : BaseViewBindingHandlerFragment() {
 
     private val binding: CoinFragmentBinding get() = requireViewBinding()
-    private lateinit var viewModel: CoinViewModel
+    private val viewModel: CoinViewModel by viewModels()
 
 
     override fun inflateViewBinding(inflater: LayoutInflater, container: ViewGroup?): ViewBinding {
         return CoinFragmentBinding.inflate(inflater, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        viewModel = ViewModelProvider(this).get(CoinViewModel::class.java)
     }
 
 }

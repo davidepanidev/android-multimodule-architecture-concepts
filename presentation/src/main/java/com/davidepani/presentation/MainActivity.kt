893c30a0ff78d4c3a4ca5524334deab0c71aba45
davide-pani <102189224+davide-pani@users.noreply.github.com>
2022-03-28 19:31:35 +0200

    Added view binding to MainActivity

--- presentation/src/main/java/com/davidepani/presentation/MainActivity.kt
+++ presentation/src/main/java/com/davidepani/presentation/MainActivity.kt
@@ -2,19 +2,19 @@ package com.davidepani.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.davidepani.presentation.ui.coin.CoinFragment
+import com.davidepani.presentation.databinding.MainActivityBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: MainActivityBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, CoinFragment.newInstance())
-                .commitNow()
-        }
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
 }

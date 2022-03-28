95511703cd6a51a85063af08466fa69d13028f3d
davide-pani <102189224+davide-pani@users.noreply.github.com>
2022-03-28 19:29:45 +0200

    Added inject annotation to CoinViewModel

--- presentation/src/main/java/com/davidepani/presentation/ui/coin/CoinViewModel.kt
+++ presentation/src/main/java/com/davidepani/presentation/ui/coin/CoinViewModel.kt
@@ -2,8 +2,9 @@ package com.davidepani.presentation.ui.coin
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class CoinViewModel : ViewModel() {
+class CoinViewModel @Inject constructor() : ViewModel() {
     // TODO: Implement the ViewModel
 }

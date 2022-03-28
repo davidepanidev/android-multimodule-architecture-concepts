00fb66cc7d02196248ebbf98d25f6ba2818a50a2
davide-pani <102189224+davide-pani@users.noreply.github.com>
2022-03-28 19:55:15 +0200

    added viewbinding to CoinFragment.kt

--- presentation/src/main/java/com/davidepani/presentation/ui/coin/CoinFragment.kt
+++ presentation/src/main/java/com/davidepani/presentation/ui/coin/CoinFragment.kt
@@ -4,31 +4,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.davidepani.presentation.R
+import androidx.viewbinding.ViewBinding
+import com.davidepani.architectures.viewbinding.BaseViewBindingHandlerFragment
+import com.davidepani.presentation.databinding.CoinFragmentBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class CoinFragment : Fragment() {
+class CoinFragment : BaseViewBindingHandlerFragment() {
 
-    companion object {
-        fun newInstance() = CoinFragment()
-    }
-
+    private val binding: CoinFragmentBinding get() = requireViewBinding()
     private lateinit var viewModel: CoinViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.coin_fragment, container, false)
+
+    override fun inflateViewBinding(inflater: LayoutInflater, container: ViewGroup?): ViewBinding {
+        return CoinFragmentBinding.inflate(inflater, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this).get(CoinViewModel::class.java)
-        // TODO: Use the ViewModel
     }
 
 }

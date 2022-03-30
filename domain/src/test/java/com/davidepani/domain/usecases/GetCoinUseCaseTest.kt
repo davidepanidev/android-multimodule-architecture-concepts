e8adcb9074ce4cf601fdc3f5995d883f5c6fa29e
davide-pani <102189224+davide-pani@users.noreply.github.com>
2022-03-30 11:50:30 +0200

    [domain] fixed GetCoinUseCaseTest.kt

--- domain/src/test/java/com/davidepani/domain/usecases/GetCoinUseCaseTest.kt
+++ domain/src/test/java/com/davidepani/domain/usecases/GetCoinUseCaseTest.kt
@@ -4,24 +4,28 @@ import com.davidepani.data.interfaces.CoinRepository
 import com.davidepani.data.models.CoinResponse
 import com.davidepani.domain.entities.Coin
 import com.davidepani.domain.mappers.DataMapper
+import io.mockk.MockKAnnotations
 import io.mockk.coEvery
 import io.mockk.every
+import io.mockk.impl.annotations.MockK
 import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 
-
+@ExperimentalCoroutinesApi
 class GetCoinUseCaseTest {
 
     private lateinit var cut: GetCoinUseCase
-    private lateinit var coinRepository: CoinRepository
-    private lateinit var dataMapper: DataMapper
+    @MockK private lateinit var coinRepository: CoinRepository
+    @MockK private lateinit var dataMapper: DataMapper
 
     @Before
     fun setUp() {
-        coinRepository = mockk()
-        dataMapper = mockk()
+        MockKAnnotations.init(this)
+
         cut = GetCoinUseCase(
             coinRepository = coinRepository,
             dataMapper = dataMapper
@@ -29,7 +33,7 @@ class GetCoinUseCaseTest {
     }
 
     @Test
-    fun correctInvoke() {
+    fun correctInvoke() = runBlockingTest {
         val coinResponse = CoinResponse(name = "Bitcoin")
         val expectedCoin = Coin(name = "Bitcoin")
 

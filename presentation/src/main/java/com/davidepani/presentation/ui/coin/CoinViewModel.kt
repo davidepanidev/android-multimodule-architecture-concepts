f066dc72e919776e826c388355cf09233632f37b
davide-pani <102189224+davide-pani@users.noreply.github.com>
2022-03-30 12:59:53 +0200

    [presentation] added method getCoin() to CoinViewModel.kt

--- presentation/src/main/java/com/davidepani/presentation/ui/coin/CoinViewModel.kt
+++ presentation/src/main/java/com/davidepani/presentation/ui/coin/CoinViewModel.kt
@@ -1,10 +1,29 @@
 package com.davidepani.presentation.ui.coin
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.davidepani.domain.entities.Coin
+import com.davidepani.domain.usecases.GetCoinUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class CoinViewModel @Inject constructor() : ViewModel() {
-    // TODO: Implement the ViewModel
+class CoinViewModel @Inject constructor(
+    private val getCoinUseCase: GetCoinUseCase
+) : ViewModel() {
+
+    private val _coinLD = MutableLiveData<Coin>()
+    val coinLD: LiveData<Coin> = _coinLD
+
+
+    fun getCoin() {
+        viewModelScope.launch {
+            val coin = getCoinUseCase()
+            _coinLD.value = coin
+        }
+    }
+
 }

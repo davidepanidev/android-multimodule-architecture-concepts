18d67d7204a7a179d232638e0d96226ff877f4ed
davide-pani <102189224+davide-pani@users.noreply.github.com>
2022-04-04 14:07:35 +0200

    [data] modified CoinResponse to match the CoinGecko api response

--- data/src/main/java/com/davidepani/data/models/CoinResponse.kt
+++ data/src/main/java/com/davidepani/data/models/CoinResponse.kt
@@ -1,5 +1,30 @@
 package com.davidepani.data.models
 
 data class CoinResponse(
-    val name: String
+    val ath: Int,
+    val ath_change_percentage: Double,
+    val ath_date: String,
+    val atl: Double,
+    val atl_change_percentage: Double,
+    val atl_date: String,
+    val circulating_supply: Int,
+    val current_price: Int,
+    val fully_diluted_valuation: Long,
+    val high_24h: Int,
+    val id: String,
+    val image: String,
+    val last_updated: String,
+    val low_24h: Int,
+    val market_cap: Long,
+    val market_cap_change_24h: Double,
+    val market_cap_change_percentage_24h: Double,
+    val market_cap_rank: Int,
+    val max_supply: Int,
+    val name: String,
+    val price_change_24h: Double,
+    val price_change_percentage_24h: Double,
+    val roi: Any,
+    val symbol: String,
+    val total_supply: Int,
+    val total_volume: Long
 )

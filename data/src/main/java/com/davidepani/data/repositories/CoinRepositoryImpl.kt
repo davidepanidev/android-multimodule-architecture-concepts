a02b388293faa0a6d9dc8c43fb1a792292f52653
davide-pani <102189224+davide-pani@users.noreply.github.com>
2022-04-04 17:09:53 +0200

    [data] changed retrieveMostCapitalizedCoin implemetation to include CoinGeckoApiService

--- data/src/main/java/com/davidepani/data/repositories/CoinRepositoryImpl.kt
+++ data/src/main/java/com/davidepani/data/repositories/CoinRepositoryImpl.kt
@@ -1,13 +1,21 @@
 package com.davidepani.data.repositories
 
+import com.davidepani.data.api.CoinGeckoApiService
 import com.davidepani.data.interfaces.CoinRepository
 import com.davidepani.data.models.CoinApiResponse
 import javax.inject.Inject
 
-class CoinRepositoryImpl @Inject constructor() : CoinRepository {
+class CoinRepositoryImpl @Inject constructor(
+    private val coinGeckoApiService: CoinGeckoApiService
+) : CoinRepository {
 
     override suspend fun retrieveMostCapitalizedCoin(): CoinApiResponse {
-        return CoinApiResponse(name = "Bitcoin")
+        return try {
+            val coinsList = coinGeckoApiService.getCoinsMarkets()
+            coinsList[0]
+        } catch(e: Exception) {
+            throw RuntimeException(e.message)
+        }
     }
 
 }

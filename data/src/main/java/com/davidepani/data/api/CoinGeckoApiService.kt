5a5ef4826c2d40d597923f685583870c1fd16369
davide-pani <102189224+davide-pani@users.noreply.github.com>
2022-04-07 12:07:06 +0200

    [data] added query parameter to getCoinsMarkets() in CoinGeckoApiService

--- data/src/main/java/com/davidepani/data/api/CoinGeckoApiService.kt
+++ data/src/main/java/com/davidepani/data/api/CoinGeckoApiService.kt
@@ -2,13 +2,13 @@ package com.davidepani.data.api
 
 import com.davidepani.data.models.CoinApiResponse
 import retrofit2.http.GET
-
+import retrofit2.http.Query
 
 
 interface CoinGeckoApiService {
 
     @GET("/api/v3/coins/markets")
-    suspend fun getCoinsMarkets(): List<CoinApiResponse>
+    suspend fun getCoinsMarkets(@Query("vs_currency") currency: String = "usd"): List<CoinApiResponse>
 
 
     companion object {

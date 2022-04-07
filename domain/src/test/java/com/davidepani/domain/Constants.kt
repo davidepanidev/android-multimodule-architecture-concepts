8cec3531d336e1dff88b0438da41ceb09137ac83
davide-pani <102189224+davide-pani@users.noreply.github.com>
2022-04-07 18:28:21 +0200

    [domain] added Constants class

---
+++ domain/src/test/java/com/davidepani/domain/Constants.kt
@@ -0,0 +1,6 @@
+package com.davidepani.domain
+
+const val bitcoinResourceJsonFilename = "coingecko_coinsmarkets_bitcoin.json"
+const val ethereumResourceJsonFilename = "coingecko_coinsmarkets_ethereum.json"
+const val coinsListResourceJsonFilename = "coingecko_coinsmarkets.json"
+
